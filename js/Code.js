9d97b6d1d607ab8afe8267e25f3c8bc20d694870
sanchit kumar <74718508+sanchit-kumar-coder@users.noreply.github.com>
2021-06-04 14:24:39 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,20 @@
+const accessCode1 = "DELHI";
+const accessCode2 = "HINDI";
+const accessCode3 = "ASHOKA";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("elhid", 200,50)
+	//variable
+	 text("Hint: capital of india !!", 100,70)
+    fill("lightblue")
+	text("inhdi", 750,80)
+	text("Hint:national language of india !!", 750,50)
+	text("shoaka", 800,220)
+	text("Hint:the chakra in the flag is called  !!", 750,200)
+// add code for displaying rest of the names and the hints.
+    
+
+}
